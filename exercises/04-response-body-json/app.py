1e8da61db5bd16c218586cbc00dac9906216e628
JosBerGon34 <infoworkjbg@gmail.com>
2024-10-14 18:35:13 +0000

    JBG

--- exercises/04-response-body-json/app.py
+++ exercises/04-response-body-json/app.py
@@ -1,4 +1,21 @@
 import requests
-
+import json
+import datetime
 response = requests.get("https://assets.breatheco.de/apis/fake/sample/time.php")
+data = response.json()
+hours = datetime["hours"]
+minutes = datetime["minutes"]
+seconds = datetime["seconds"]
+
+
+datetime_str = data("date_time")
+datetime_object = datetime.fromisoformat(datetime_str)
+
+hours = datetime_object.hours
+minutes = datetime_object.minutes
+seconds = datetime_object.seconds
 print(response.text)
+print(f"Current time: {hours} horas {minutes} minutos y {seconds} segundos")
+
+
+
